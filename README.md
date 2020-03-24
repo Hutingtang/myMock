e761b202fafcdd248cf7bdb2ff1a1ab97aacb791
cxl <shuanglongchen@yeah.net>
2020-03-24 15:43:29 +0800

    change

--- README.md
+++
@@ -1,2 +0,0 @@
-# myMock
-mock Data
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+let Mock = require('mockjs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/test', (req, res) => {
+  
+});
 module.exports = router;
